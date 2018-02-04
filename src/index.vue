975a4da372d4ee95fd73a53ba3a4c8817012f2e7
NowUI <nowui@163.com>
2018-02-04 18:09:17 +0800

    初始化项目

---
+++ src/index.vue
@@ -0,0 +1,21 @@
+<style src="./index.css" scoped></style>
+<template src="./index.html"></template>
+
+<script>
+    export default {
+        data() {
+            return {
+                posters: [
+                    'http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/320/h/320',
+                    'http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/320/h/320',
+                    'http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/320/h/320',
+                    'http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/320/h/320',
+                    'http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/320/h/320'
+                ]
+            }
+        },
+        created() {
+
+        }
+    }
+</script>
---
+++ src/view/my/index.vue
@@ -0,0 +1,13 @@
+<style src="./index.css" scoped></style>
+<template src="./index.html"></template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+
+        }
+    }
+</script>
