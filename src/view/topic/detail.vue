e0fb0cda82a0d1b58203bed927cbc9201b438c60
NowUI <nowui@163.com>
2018-02-20 00:54:08 +0800

    页面布局

--- src/view/topic/detail.vue
+++ src/view/topic/detail.vue
@@ -2,34 +2,25 @@
 <template src="./detail.html"></template>
 
 <script>
-    import {WxcCell} from 'weex-ui'
+    import {WxcCell, WxcLightbox} from 'weex-ui'
 
     import mixins from '../../mixins/index';
 
     export default {
         components: {
-            WxcCell
+            WxcCell,
+            WxcLightbox
         },
         mixins: [mixins],
         data() {
             return {
-                feedbackList: [{
-
-                }, {
-
-
-                }, {
-
-
-                }, {
-
-
-                }, {
-
-
-                }, {
-
-                }]
+                isShow: false,
+                imageList: [
+                    {src: 'https://gd2.alicdn.com/bao/uploaded/i2/T14H1LFwBcXXXXXXXX_!!0-item_pic.jpg'},
+                    {src: 'https://gd1.alicdn.com/bao/uploaded/i1/TB1PXJCJFXXXXciXFXXXXXXXXXX_!!0-item_pic.jpg'},
+                    {src: 'https://gd3.alicdn.com/bao/uploaded/i3/TB1x6hYLXXXXXazXVXXXXXXXXXX_!!0-item_pic.jpg'}
+                ],
+                feedbackList: [{}, {}, {}, {}, {}, {}]
             }
         },
         created() {
@@ -38,6 +29,12 @@
         methods: {
             handleHomepage() {
                 this.push('/view/member/homepage.html');
+            },
+            handleOpenImage() {
+                this.isShow = true;
+            },
+            handleCloseImage() {
+                this.isShow = false;
             }
         }
     }
