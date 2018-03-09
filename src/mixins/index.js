0c6f814bc7a39c07e591b033787c4cbaa44ef1df
NowUI <nowui@163.com>
2018-03-09 15:05:18 +0800

    页面布局

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -10,8 +10,8 @@ export default {
     data() {
         return {
             pageHeight: 0,
-            host: 'http://192.168.2.125:8080',
-            // host: 'http://118.31.229.16:80',
+            // host: 'http://192.168.2.125:8080',
+            host: 'http://118.31.229.16:80',
             imageHost: 'http://118.31.229.16:80',
             // imageHost: 'http://localhost:8080',
             appId: 'df2078d6c9eb46babb0df957127273ab',
